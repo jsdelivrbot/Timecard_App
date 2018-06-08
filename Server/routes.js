266dd0e03aad166f5c19d4d191eac6dbe34252ae
mayur_d_developer <dmayur57@gmail.com>
2018-06-08 19:18:00 +0530

    adding fetch timesheet entries to routes

--- Server/routes.js
+++ Server/routes.js
@@ -1,8 +1,9 @@
 // var PV = require('./passverification');
 var FT = require('./controllers/fetchTasks');
+var FTSE = require('./controllers/fetchTSEntries');
 var ST = require('./controllers/submitTSEntries');
 
-module.exports = function(app,passport) {
+module.exports = function (app, passport) {
 
     // =====================================
     // HOME PAGE (with login links) ========
@@ -10,42 +11,46 @@ module.exports = function(app,passport) {
     // app.get('/', function(req, res) {
     //     res.render('pages/index.ejs'); // load the index.ejs file
     // });
-    app.get('/blank', function(req, res){
+    app.get('/blank', function (req, res) {
         res.render('pages/blank');
-  });
+    });
 
     // =====================================
     // LOGIN ===============================
     // =====================================
     // show the login form
-    app.get('/', function(req, res) {
+    app.get('/', function (req, res) {
 
         // render the page and pass in any flash data if it exists
-        res.render('pages/login.ejs', { message: req.flash('loginMessage') }); 
+        res.render('pages/login.ejs', {
+            message: req.flash('loginMessage')
+        });
     });
 
     // process the login form
     app.post('/login', passport.authenticate('local-login', {
-        successRedirect : '/home', // redirect to the secure profile section
-        failureRedirect : '/login', // redirect back to the signup page if there is an error
-        failureFlash : true // allow flash messages
+        successRedirect: '/home', // redirect to the secure profile section
+        failureRedirect: '/login', // redirect back to the signup page if there is an error
+        failureFlash: true // allow flash messages
     }));
 
     // =====================================
     // SIGNUP ==============================
     // =====================================
     // show the signup form
-    app.get('/signup', function(req, res) {
+    app.get('/signup', function (req, res) {
 
         // render the page and pass in any flash data if it exists
-        res.render('pages/signup.ejs', { message: req.flash('signupMessage') });
+        res.render('pages/signup.ejs', {
+            message: req.flash('signupMessage')
+        });
     });
 
     // process the signup form
     app.post('/signup', passport.authenticate('local-signup', {
-        successRedirect : '/home', // redirect to the secure profile section
-        failureRedirect : '/signup', // redirect back to the signup page if there is an error
-        failureFlash : true // allow flash messages
+        successRedirect: '/home', // redirect to the secure profile section
+        failureRedirect: '/signup', // redirect back to the signup page if there is an error
+        failureFlash: true // allow flash messages
     }));
 
     // =====================================
@@ -53,10 +58,9 @@ module.exports = function(app,passport) {
     // =====================================
     // we will want this protected so you have to be logged in to visit
     // we will use route middleware to verify this (the isLoggedIn function)
-    app.get('/home', isLoggedIn, function(req, res) {
-        res.render('pages/home'
-        , {
-            user : req.user // get the user out of session and pass to template
+    app.get('/home', isLoggedIn, function (req, res) {
+        res.render('pages/home', {
+            user: req.user // get the user out of session and pass to template
         });
     });
 
@@ -65,60 +69,75 @@ module.exports = function(app,passport) {
     // =====================================
     // we will want this protected so you have to be logged in to visit
     // we will use route middleware to verify this (the isLoggedIn function)
-    app.get('/viewEditlogs', isLoggedIn, function(req, res) {
-        res.render('pages/viewEditLogs'
-        , {
-            user : req.user // get the user out of session and pass to template
+    app.get('/viewEditlogs', isLoggedIn, function (req, res) {
+        res.render('pages/viewEditLogs', {
+            user: req.user // get the user out of session and pass to template
         });
     });
 
     // =====================================
     // LOGOUT ==============================
     // =====================================
-    app.get('/logout', function(req, res) {
+    app.get('/logout', function (req, res) {
         req.logout();
         res.redirect('/');
     });
 
 
 
-    
+
 
     // =====================================
     // FETCH TASKS =========================
     // =====================================
-  app.post('/fetchTasks', function(req, res){
-    //   console.log(req.body['username']);
-    //   console.log(req.body['date']);
-      
-    FT.getUserTasks(req.body['username'],req.body['date'],(err,userTasks) => {
-        if(!userTasks){
-            res.status(400).send(err);
-        }else{
-            res.status(200).send(userTasks);
-        }
+    app.post('/fetchTasks', function (req, res) {
+        //   console.log(req.body['username']);
+        //   console.log(req.body['date']);
+
+        FT.getUserTasks(req.body['username'], req.body['date'], (err, userTasks) => {
+            if (!userTasks) {
+                res.status(400).send(err);
+            } else {
+                res.status(200).send(userTasks);
+            }
+        });
+
+
     });
-    
-    
-});
+    // =====================================
+    // FETCH TS_ENTRIES =========================
+    // =====================================
+    app.post('/fetchTSEntries', function (req, res) {
+        //   console.log(req.body['username']);
+        //   console.log(req.body['date']);
+
+        FTSE.getUserTSEntries(req.body['username'], req.body['date'], (err, userTSEntries) => {
+            if (!userTSEntries) {
+                res.status(400).send(err);
+            } else {
+                res.status(200).send(userTSEntries);
+            }
+        });
+    });
 
     // =====================================
     // submit TS Entries ===================
     // =====================================
-    app.post('/submit_TS', function(req, res){
+    app.post('/submit_TS', function (req, res) {
         //   console.log(req.body['username']);
-          console.log(req.body['effort']);
-          
-        ST.enterTS(req.body['username'],req.body['effort'],(err,userTS) => {
-            if(!userTS){
+        console.log(req.body['effort']);
+
+        ST.enterTS(req.body['username'], req.body['effort'], (err, userTS) => {
+            if (!userTS) {
                 res.status(400).send(err);
-            }else{
+            } else {
                 res.status(200).send(userTS);
             }
         });
-        
-        
+
+
     });
+
 
 
 };
@@ -133,6 +152,3 @@ function isLoggedIn(req, res, next) {
     // if they aren't redirect them to the home page
     res.redirect('/');
 }
-
-
-    
