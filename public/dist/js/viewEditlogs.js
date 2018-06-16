77d02c9ec394b670204bdf8b4d8327b96247c4e8
mayur_d_developer <dmayur57@gmail.com>
2018-06-16 17:34:00 +0530

    Changes for adding action buttons and editables

--- public/dist/js/viewEditlogs.js
+++ public/dist/js/viewEditlogs.js
@@ -46,11 +46,67 @@ function addTSEntriesToList(res) {
     $("#effortEntriesListTable tbody").children().remove();
     tasks.rows.forEach(TSE => {
         TSE_array.push(TSE);
-        $("#effortEntriesListTable tbody").append('<tr><td>'+TSE.srNumber+'</td><td>'+TSE.effortHrs+'</td></tr>');
+        var row_id = random_id();
+        markup='<tr row_id="'+row_id+'">'
+        +'<td><div col_name="srNumber">'+TSE.srNumber+'</td>'
+        +'<td><div class="row_data" edit_type="click" col_name="effortHrs">'+TSE.effortHrs+'</td>'
+        // show edit button
+        +'<td><span class="btn_edit"><a href="#" class="btn btn-link " row_id="'+row_id+'" > Edit</a>'+'</span>'
+        // Show save,cancel and delete buttons if edit is clicked 
+        +'<span class="btn_save"><a href="#" class="btn btn-link"  row_id="'+row_id+'"> Save</a> |'+'</span>'
+        +'<span class="btn_cancel"><a href="#" class="btn btn-link"  row_id="'+row_id+'"> Cancel</a> |'+'</td></span>'
+        +'</tr>'
+        $("#effortEntriesListTable tbody").append(markup);
+        $(document).find('.btn_save').hide();
+	    $(document).find('.btn_cancel').hide();
     });
+
     // console.log(SR_array);
     removeLoaderFor('effort_dt_sel');
     
     $('#effort_entries_list').show();
 }
+
+function actionMarkups(){
+    return "";
+}
+
+
+var random_id = function  () 
+	{
+		var id_num = Math.random().toString(9).substr(2,3);
+		var id_str = Math.random().toString(36).substr(2);
+		
+		return id_num + id_str;
+    }
+    
+$(document).on('click', '.btn_edit', function(event){
+    event.preventDefault();
+    var tbl_row = $(this).closest('tr');
+
+    var row_id = tbl_row.attr('row_id');
+
+    tbl_row.find('.btn_save').show();
+    tbl_row.find('.btn_cancel').show();
+
+    //hide edit button
+    tbl_row.find('.btn_edit').hide(); 
+
+    //make the whole row editable
+    tbl_row.find('.row_data')
+    .attr('contenteditable', 'true')
+    .attr('edit_type', 'button')
+    .addClass('bg-warning')
+    .css('padding','3px')
+
+    //--->add the original entry > start
+    tbl_row.find('.row_data').each(function(index, val) 
+    {  
+        //this will help in case user decided to click on cancel button
+        $(this).attr('original_entry', $(this).html());
+    }); 		
+    //--->add the original entry > end
+
+});
+//--->button > edit > end
 
