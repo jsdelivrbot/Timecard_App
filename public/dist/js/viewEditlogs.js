46ceb0692753fd4494054168c10c866d60725f8e
mayur_d_developer <dmayur57@gmail.com>
2018-06-25 14:32:00 +0530

    changes to handle save, update, delete Time entries

--- public/dist/js/viewEditlogs.js
+++ public/dist/js/viewEditlogs.js
@@ -1,5 +1,6 @@
 var selected_date;
 var SR_array = [];
+var TSE_array = [];
 
 $(document).ready(function () {
     $('#effort_entries_list').hide();
@@ -93,12 +94,13 @@ function addTSEntriesToList(res) {
         markup = '<tr row_id="' + row_id + '">' +
             '<td><div class="row_SR_Num" col_name="srNumber">' + TSE.srNumber + '</td>' +
             '<td><div class="row_data" edit_type="click" col_name="effortHrs">' + TSE.effortHrs + '</td>'
-            // show edit button
+            // show edit and delete button
             +
-            '<td><span class="btn_edit"><a href="#" class="btn btn-link " row_id="' + row_id + '" > Edit</a>' + '</span>'
-            // Show save,cancel and delete buttons if edit is clicked 
+            '<td><span class="btn_edit"><a href="#" class="btn btn-link " row_id="' + row_id + '" > Edit</a> |' + '</span>' +
+            '<span class="btn_delete"><a href="#" class="btn btn-link " row_id="' + row_id + '" > Delete</a>' + '</span>'
+            // Show update,cancel buttons if edit is clicked 
             +
-            '<span class="btn_save"><a href="#" class="btn btn-link"  row_id="' + row_id + '"> Save</a> |' + '</span>' +
+            '<span class="btn_save"><a href="#" class="btn btn-link"  row_id="' + row_id + '"> Update</a> |' + '</span>' +
             '<span class="btn_cancel"><a href="#" class="btn btn-link"  row_id="' + row_id + '"> Cancel</a> |' + '</td></span>' +
             '</tr>'
         $("#effortEntriesListTable tbody").append(markup);
@@ -108,8 +110,54 @@ function addTSEntriesToList(res) {
 
     // console.log(SR_array);
     removeLoaderFor('effort_dt_sel');
+    removeLoaderFor('effort_entries_list')
 
     $('#effort_entries_list').show();
+}
+
+function handleSave(user, effort) {
+    showLoaderFor('effort_entry_form');
+    showLoaderFor('effort_dt_sel');
+    showLoaderFor('effort_entries_list');
+    console.log('submit_TS' + 'triggerd');
+    $.ajax({
+        url: "/submit_TS",
+        type: "POST",
+        data: {
+            username: user.name,
+            effort: [effort]
+        },
+        success: function (res, status, xhr, $form) {
+            console.log('submit_TS : success' + 'triggerd');
+            // console.log(res);
+            show_alert("Time entry saved", "success", 4000);
+            removeLoaderFor('effort_entry_form');
+            removeLoaderFor('effort_dt_sel');
+            fetchTSEntries(user.name, selected_date);
+            clearTimeEntryForm();
+        },
+        error: function (e) {
+            show_alert("There was an error while recording your timesheets.\n Please connect with the admin to resolve the issue", "danger", 4000);
+        }
+    });
+}
+
+function validateNewEntry(sr_number, hrs_spent) {
+
+    if (sr_number == 'Select Task') {
+        return false;
+    }
+
+
+    if (hrs_spent == NaN || hrs_spent == 0 || hrs_spent < 0 || hrs_spent > 24 || hrs_spent == '') {
+        return false;
+    }
+    return true;
+}
+
+function clearTimeEntryForm() {
+    $('#hrsTmst').val('');
+    fetchTasks(user.name, selected_date);
 }
 
 $(document).on('click', '.btn_edit', function (event) {
@@ -123,6 +171,7 @@ $(document).on('click', '.btn_edit', function (event) {
 
     //hide edit button
     tbl_row.find('.btn_edit').hide();
+    tbl_row.find('.btn_delete').hide();
 
     //make the whole row editable
     tbl_row.find('.row_data')
@@ -155,6 +204,7 @@ $(document).on('click', '.btn_cancel', function (event) {
 
     //show edit button
     tbl_row.find('.btn_edit').show();
+    tbl_row.find('.btn_delete').show();
 
     //make the whole row uneditable
     tbl_row.find('.row_data')
@@ -170,7 +220,7 @@ $(document).on('click', '.btn_cancel', function (event) {
 //--->button > cancel > end
 
 
-//--->save whole row entery > start	
+//--->update whole row entery > start	
 $(document).on('click', '.btn_save', function (event) {
     event.preventDefault();
     var tbl_row = $(this).closest('tr');
@@ -184,6 +234,7 @@ $(document).on('click', '.btn_save', function (event) {
 
     //show edit button
     tbl_row.find('.btn_edit').show();
+    tbl_row.find('.btn_delete').show();
 
 
     //make the whole row editable
@@ -219,11 +270,145 @@ $(document).on('click', '.btn_save', function (event) {
     console.log(arr);
 
     //out put to show
-    show_alert(JSON.stringify(arr), 'success', 2000)
+    // show_alert(JSON.stringify(arr), 'success', 4000)
     // $('.post_msg').html('<pre class="bg-success">' + JSON.stringify(arr, null, 2) + '</pre>');
 
+    handleUpdate(arr);
 
 
 
 });
-//--->save whole row entery > end
+//--->update whole row entery > end
+
+//--->delete whole row entery > start	
+$(document).on('click', '.btn_delete', function (event) {
+    event.preventDefault();
+    var tbl_row = $(this).closest('tr');
+
+    changeConfimationModal("Are you sure to delete this time entry", "Delete", "Cancel");
+    $('#confirm').modal({
+            backdrop: 'static',
+            keyboard: false
+        })
+        .one('click', '#confirm_btn_positive', function (e) {
+            
+
+            // var row_id = tbl_row.attr('row_id');
+
+            //--->get row data > start
+            var del_arr = {};
+            tbl_row.find('.row_data').each(function (index, val) {
+                var col_name = $(this).attr('col_name');
+                var col_val = $(this).html();
+                del_arr[col_name] = col_val;
+            });
+            //--->get row data > end
+
+            tbl_row.find('.row_SR_Num').each(function (index, val) {
+                var col_name = $(this).attr('col_name');
+                var col_val = $(this).html();
+                del_arr[col_name] = col_val;
+            });
+
+
+            //use the "del_arr"	object for your ajax call
+            $.extend(del_arr, {
+                username: user.name
+            });
+            $.extend(del_arr, {
+                date: selected_date
+            });
+            console.log(del_arr);
+
+            //out put to show
+            // show_alert(JSON.stringify(del_arr), 'success', 4000)
+            // $('.post_msg').html('<pre class="bg-success">' + JSON.stringify(del_arr, null, 2) + '</pre>');
+
+            handleDelete(del_arr);
+        });
+
+
+});
+//--->delete whole row entry > end
+
+//--->save new entry > start	
+$(document).on('click', '#save', function (event) {
+    event.preventDefault();
+    const sr_number = $('#selectTask option:selected').val();
+    const hrs_spent = $('#hrsTmst').val();
+
+    if (!validateNewEntry(sr_number, hrs_spent)) {
+        show_alert("Please select and enter valid task and hours spent.", "danger", 2000);
+        return false;
+    }
+
+    effort = {
+        date: `${selected_date}`,
+        sr_number: `${sr_number}`,
+        hrs_spent: parseFloat(hrs_spent)
+    };
+    console.log(user, [effort]);
+    handleSave(user, effort);
+
+
+
+});
+//--->save  new entery > end
+
+function handleUpdate(updated_effort) {
+    showLoaderFor('effort_entry_form');
+    showLoaderFor('effort_dt_sel');
+    showLoaderFor('effort_entries_list');
+    console.log('update_TS ' + 'triggerd');
+    $.ajax({
+        url: "/update_TS",
+        type: "POST",
+        data: {
+            username: user.name,
+            date: updated_effort.date,
+            srNumber: updated_effort.srNumber,
+            effortHrs: updated_effort.effortHrs
+        },
+        success: function (res, status, xhr, $form) {
+            console.log('update_TS : success ' + 'triggerd');
+            // console.log(res);
+            show_alert("Time entry updated", "success", 4000);
+            removeLoaderFor('effort_entry_form');
+            removeLoaderFor('effort_dt_sel');
+            fetchTSEntries(user.name, selected_date);
+            clearTimeEntryForm();
+        },
+        error: function (e) {
+            show_alert("There was an error while updating your timesheets.\n Please Try agin or connect with the admin to resolve the issue", "danger", 4000);
+        }
+    });
+}
+
+function handleDelete(delete_effort) {
+    showLoaderFor('effort_entry_form');
+    showLoaderFor('effort_dt_sel');
+    showLoaderFor('effort_entries_list');
+    console.log('delete_TS ' + 'triggerd');
+    $.ajax({
+        url: "/delete_TS",
+        type: "POST",
+        data: {
+            username: user.name,
+            date: delete_effort.date,
+            srNumber: delete_effort.srNumber
+        },
+        success: function (res, status, xhr, $form) {
+            console.log('delete_TS : success ' + 'triggerd');
+            // console.log(res);
+            show_alert("Time entry deleted", "success", 4000);
+            removeLoaderFor('effort_entry_form');
+            removeLoaderFor('effort_dt_sel');
+            fetchTSEntries(user.name, selected_date);
+            clearTimeEntryForm();
+        },
+        error: function (e) {
+            console.log(e);
+            show_alert("There was an error while deleting your time entry.\n Please retry again or connect with the admin to resolve the issue", "danger", 4000);
+        }
+    });
+}
