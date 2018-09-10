0293119a58198352fc631a75cce34cbf2a5c7f4e
mayur_d_developer <dmayur57@gmail.com>
2018-09-10 10:30:00 +0530

    changes for column names and datatypes

--- Server/model/ts_entry.js
+++ Server/model/ts_entry.js
@@ -5,7 +5,7 @@ const db = require('../controllers/db');
 
 exports.insertTSEntries =  (ts_entries, DBdone)=>{
     const query = {
-        text: `INSERT INTO "srtracker"."timesheet_entry"("date","srNumber","effortHrs", "assignee","assignee_sso") VALUES ${ts_entries}`
+        text: `INSERT INTO "srtracker"."timesheet_entry"("effort_dt","task","effortHrs", "assignee_name","assignee_sso") VALUES ${ts_entries}`
       }
       
     db.query(query, (err, res) => {
@@ -19,11 +19,11 @@ exports.insertTSEntries =  (ts_entries, DBdone)=>{
     });
 };
 
-// find all ts_entries based on assignee and date 
-exports.findTSEntriesOnDate =  (assignee, sso, date, DBdone)=>{
+// find all ts_entries based on assignee and effort_dt 
+exports.findTSEntriesOnDate =  (assignee, sso, effort_dt, DBdone)=>{
     const query = {
-        text: 'SELECT * FROM srtracker.timesheet_entry WHERE assignee_sso=$1 and date::timestamp::date = $2',
-        values: [sso, date]
+        text: `SELECT * FROM srtracker.timesheet_entry WHERE assignee_sso=$1 and effort_dt::timestamp::date = $2`,
+        values: [sso, effort_dt]
       }
       
     db.query(query, (err, res) => {
@@ -37,11 +37,11 @@ exports.findTSEntriesOnDate =  (assignee, sso, date, DBdone)=>{
     });
 };
 
-//update ts_entry based on user,task,date
-exports.updateTSEntries =  (assignee, sso, date, task,hrs,DBdone)=>{
+//update ts_entry based on user,task,effort_dt
+exports.updateTSEntries =  (assignee, sso, effort_dt, task,hrs,DBdone)=>{
     const query = {
-        text: 'UPDATE srtracker.timesheet_entry SET "effortHrs"=$3 WHERE assignee_sso=$4 and date::timestamp::date = $1 and "srNumber"=$2' ,
-        values: [date,task,hrs, sso]
+        text: `UPDATE srtracker.timesheet_entry SET "effortHrs"=$3 WHERE assignee_sso=$4 and effort_dt::timestamp::date = $1 and task=$2` ,
+        values: [effort_dt,task,hrs, sso]
       }
       
     db.query(query, (err, res) => {
@@ -55,11 +55,11 @@ exports.updateTSEntries =  (assignee, sso, date, task,hrs,DBdone)=>{
     });
 };
 
-//delete ts_entry based on user,task,date
-exports.deleteTSEntries =  (assignee,sso,  date, task,DBdone)=>{
+//delete ts_entry based on user,task,effort_dt
+exports.deleteTSEntries =  (assignee,sso,  effort_dt, task,DBdone)=>{
     const query = {
-        text: 'DELETE FROM srtracker.timesheet_entry WHERE assignee_sso=$3 and date::timestamp::date = $1 and "srNumber"=$2' ,
-        values: [date,task,sso]
+        text: `DELETE FROM srtracker.timesheet_entry WHERE assignee_sso=$3 and effort_dt::timestamp::date = $1 and task=$2` ,
+        values: [effort_dt,task,sso]
       }
       
     db.query(query, (err, res) => {
