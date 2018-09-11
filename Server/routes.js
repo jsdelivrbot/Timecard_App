f4449748141e2ed099ba5fe02a8acf3cf5b58a03
mayur_d_developer <dmayur57@gmail.com>
2018-09-11 09:00:00 +0530

    basic route changes

--- Server/routes.js
+++ Server/routes.js
@@ -50,7 +50,7 @@ module.exports = function (app, passport) {
 
     // process the signup form
     app.post('/signup', passport.authenticate('local-signup', {
-        successRedirect: '/home', // redirect to the secure profile section
+        successRedirect: '/', // redirect to the secure profile section
         failureRedirect: '/signup', // redirect back to the signup page if there is an error
         failureFlash: true // allow flash messages
     }));
@@ -61,7 +61,7 @@ module.exports = function (app, passport) {
     // we will want this protected so you have to be logged in to visit
     // we will use route middleware to verify this (the isLoggedIn function)
     app.get('/home', isLoggedIn, function (req, res) {
-        res.render('pages/home', {
+        res.render('pages/viewEditLogs', {
             user: req.user // get the user out of session and pass to template
         });
     });
